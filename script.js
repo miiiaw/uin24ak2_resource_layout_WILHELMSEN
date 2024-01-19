4207e84a68560721cca472f77ee55a88424a9b82
miiiaw <141754937+miiiaw@users.noreply.github.com>
2024-01-19 16:55:55 +0100

    Oppdatert
    JS ferdig, CSS og index.html ferdig med Google fonts

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Last inn HTML ved innlasting av siden
+document.addEventListener("DOMContentLoaded", getObject(`HTML`)
+)
+
 // Array til konsoll
 console.log(resources)
 
-// Variabler for lagring av HTML 
+// Variabel for lagring av HTML i nav
 let navHTML = ""
-let mainHTML = ""
 
 
 // Legg til array-items i navHTML
 resources.map(items => navHTML += 
-    `<li>${items.category}</li>`)
+    `<li onclick="getObject('${items.category}')">${items.category}</li>`)
 
 // Legg navHTML i nav
 const nav = document.getElementsByTagName("nav")
@@ -18,33 +21,31 @@ nav[0].innerHTML = navHTML
 
 // Hent ut spesifisert objekt
 function getObject(categoryTitle) {
-    return resources.filter(resource => resource.category === categoryTitle)
-    // MÅ ENDRES - BRUK IF ... RETURN ...
+    // Variabel for lagring av HTML i main
+    let mainHTML = ""
+    // Hent ut spesifisert objekt med filter
+    let objects = resources.filter(resource => resource.category === categoryTitle)
+
+    // Hent ut items fra objekt og konkatiner
+    objects.map(mainItems => {
+        mainHTML += `<h1>${mainItems.category}</h1>
+        <p>${mainItems.text}</p>`
+        
+        mainItems.sources.map(sources => {
+            mainHTML += `<li><a href="${sources.url}">${sources.title}</a></li>`
+        })
+    })
+
+    console.log(objects);
+
+    // Vis i main
+    const main = document.getElementsByTagName("main")
+    main[0].innerHTML = mainHTML
 }
 
-// Lagre objekt i variabel
-let objects = getObject("HTML");
-
-console.log(objects);
-
-// Hent ut items fra objekt og konkatiner
-objects.map(mainItems => {
-    mainHTML += `<h1>${mainItems.category}</h1>
-    <p>${mainItems.text}</p>`
-    
-    mainItems.sources.map(sources => {
-        mainHTML += `<li><a href="${sources.url}">${sources.title}</a></li>`
-    })
-})
-
-// Vis i main
-const main = document.getElementsByTagName("main")
-main[0].innerHTML = mainHTML
 
 
-/*
-// Eventlistener til liste-element i nav
-document.querySelectorAll("nav li").forEach(li => {
-    li.addEventListener("click", getObject)
-}) 
-*/
+
+
+
+
