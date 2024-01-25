1db182d71eb40c310b6c4cc228cbdebb808fc653
miiiaw <141754937+miiiaw@users.noreply.github.com>
2024-01-25 12:40:16 +0100

    Oppdatert js og css
    Ryddet i kode. Lagt til funksjon for å legge til klasse på klikket element.

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-// Last inn HTML ved innlasting av siden
-document.addEventListener("DOMContentLoaded", getObject(`HTML`)
-)
+// Last inn HTML ved innlasting av siden (Hjelp fra ChatGPT for å finne rett eventListener). Vet ikke om dette er rett måte å gjøre det på, men det funker :)
+document.addEventListener("DOMContentLoaded", getObject('HTML'))
+
 
 // Array til konsoll
-console.log(resources)
+//console.log(resources)
 
 // Variabel for lagring av HTML i nav
-let navHTML = ""
-
+let navHTML = `<ul>`
 
 // Legg til array-items i navHTML
-resources.map(items => navHTML += 
-    `<li onclick="getObject('${items.category}')">${items.category}</li>`)
+resources.map(item => {
+    navHTML += `<li onclick="getObject('${item.category}'); addActive(this);">${item.category}</li>`
+})
 
 // Legg navHTML i nav
-const nav = document.getElementsByTagName("nav")
-nav[0].innerHTML = navHTML
+const nav = document.getElementsByTagName("nav")[0]
+nav.innerHTML += navHTML + `</ul>`
 
 
+// Legg til klasse på klikket element (Hjelp fra ChatGPT med syntax'en for å forstå hvordan jeg kan legge til klassen på det klikkede elementet uten å referere til en ID el.)
+function addActive(clickedElement) {
+    // Fjern klassen fra alle li-elementene
+    const listItems = document.querySelectorAll("nav ul li")
+    listItems.forEach(li => li.classList.remove("active"))
 
-// Hent ut spesifisert objekt
+    // Legg til klasse på klikket element
+    clickedElement.classList.add("active")
+}
+
+
+// Hent ut spesifisert/valgt objekt
 function getObject(categoryTitle) {
     // Variabel for lagring av HTML i main
     let mainHTML = ""
-    // Hent ut spesifisert objekt med filter
+    // Hent ut spesifisert objekt
     let objects = resources.filter(resource => resource.category === categoryTitle)
 
     // Hent ut items fra objekt og konkatiner
     objects.map(mainItems => {
-        mainHTML += `<h1>${mainItems.category}</h1>
-        <p>${mainItems.text}</p>`
+        mainHTML += `<h1>${mainItems.category}</h1><p>${mainItems.text}</p>`
         
         mainItems.sources.map(sources => {
             mainHTML += `<li><a href="${sources.url}">${sources.title}</a></li>`
         })
     })
 
-    console.log(objects);
-
     // Vis i main
-    const main = document.getElementsByTagName("main")
-    main[0].innerHTML = mainHTML
+    const main = document.getElementsByTagName("main")[0]
+    main.innerHTML = mainHTML
 }
-
-
-
-
-
-
-
